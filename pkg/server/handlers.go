b6a4fc48c89762a76594ded05e4c70214fb526f0
Roman SHamagin <roman.shamagin@7vpn.com>
2023-08-12 06:26:39 +0300

    use h265

--- pkg/server/handlers.go
+++ pkg/server/handlers.go
@@ -125,7 +125,7 @@ func (c *Config) m3u8ReverseProxy(ctx *gin.Context) {
 	outputFile := dir + "/" + newName
 
 	// Сжатие файла с помощью ffmpeg
-	cmd := exec.Command("ffmpeg", "-i", filepath, "-b:v", "800k", "-b:a", "128k", "-preset", "medium", outputFile)
+	cmd := exec.Command("ffmpeg", "-i", filepath, "-vf", "scale=720:480", "-c:v", "libx265", "-b:v", "600k", "-b:a", "128k", "-preset", "medium", outputFile)
 	err = cmd.Run()
 	if err != nil {
 		log.Fatalf("Ошибка при сжатии файла: %v", err)
